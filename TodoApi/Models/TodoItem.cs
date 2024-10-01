6b7c9ebcf6f54eefdc6c26a1032a321bdcc898ca
Itsguri31 <gurkirats0001@gmail.com>
2024-10-01 14:49:08 +0000

    Model folder added

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TodoApiList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; } 
+
+        public DateTime DueDate { get; set; } 
+
+        public DateTime? CompletedDate { get; set; } 
+
+        public string Description { get; set; } 
+    }
+}
