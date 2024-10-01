c06f4f65a6f36b7955324bcf02dcc656f929973a
Itsguri31 <gurkirats0001@gmail.com>
2024-10-01 15:00:42 +0000

    Added a database context

---
+++ TodoApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApiList.Models;
+
+namespace TodoApiList.Data;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
